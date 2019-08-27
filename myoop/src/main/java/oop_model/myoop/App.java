a8e508ac54216a9fcc3268eba99ad739e9e31e5b
lamarcomer <lamar.comer@hotmail.com>
2019-08-27 14:10:50 -0400

    my commit message

---
+++ myoop/src/main/java/oop_model/myoop/App.java
@@ -0,0 +1,13 @@
+package oop_model.myoop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
